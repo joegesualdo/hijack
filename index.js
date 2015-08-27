20f540b8a7bc5753ed3bcf36bd59bfe8ecce5408
Joe Gesualdo <joegesualdo@gmail.com>
2015-08-27 17:33:47 -0400

    Fix bug

--- index.js
+++ index.js
@@ -2,22 +2,33 @@ var Hijack = (function(){
   function before(fnString, beforeFn){
     // These should be some error checking 
     
+    originalFn = getFunctionByName(fnString, window)
     Manipulate.redefineFunction(fnString, function(){
       beforeFn();
-      executeFunctionByName(fnString, window);
+      originalFn();
+      // executeFunctionByName(fnString, window);
     }, window)
   }
 
-  // TODO: Make this a seperate module
-  function executeFunctionByName(functionName, context /*, args */) {
-    var args = [].slice.call(arguments).splice(2);
+  function getFunctionByName(functionName, context /*, args */) {
+   
     var namespaces = functionName.split(".");
     var func = namespaces.pop();
     for(var i = 0; i < namespaces.length; i++) {
-      context = context[namespaces[i]];
-    }
-    return context[func].apply(this, args);
+        context = context[namespaces[i]];
+      }
+    return context[func];
   }
+  // TODO: Make this a seperate module
+  // function executeFunctionByName(functionName, context /*, args */) {
+  //   var args = [].slice.call(arguments).splice(2);
+  //   var namespaces = functionName.split(".");
+  //   var func = namespaces.pop();
+  //   for(var i = 0; i < namespaces.length; i++) {
+  //     context = context[namespaces[i]];
+  //   }
+  //   return context[func].apply(this, args);
+  // }
   return {
     before: before
   }
