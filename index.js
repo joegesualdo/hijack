835e0669dccff997e2bb6457d90b02a2a671a725
Joe Gesualdo <joegesualdo@gmail.com>
2015-08-27 14:53:52 -0400

    Change structure

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-export function before(fnString, beforeFn){
-  fn = window[fnString]
-  fnCopy = window[fnString]
-  if(typeof fn === 'function'){
-    window[fnString] = function(){
-      beforeFn();
-      fnCopy();
+var Hijack = (function(){
+  function before(fnString, beforeFn){
+    fn = window[fnString]
+    fnCopy = window[fnString]
+    if(typeof fn === 'function'){
+      window[fnString] = function(){
+        beforeFn();
+        fnCopy();
+      }
     }
   }
-}
+  return {
+    before: before;
+  }
+})()
